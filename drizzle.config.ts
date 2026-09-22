5377ad9cc5f302d0dc88f8e39353478888f97e48
arya-satwika <aryastwk95@gmail.com>
2026-09-22 22:07:06 +0700

    migrated to pnpm

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./src/lib/server/schema.ts",
   out: "./drizzle",
-  dbCredentials:{ url: import.meta.env.VITE_DATABASE_URL }
+  dbCredentials:{ url: process.env.VITE_DATABASE_URL! }
 
 })
