00acf453b083e949e500b2fb616ba32cd1e694a0
arya-satwika <aryastwk95@gmail.com>
2026-09-22 20:05:07 +0700

    handoff to laptop

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -1,12 +1,47 @@
-import { date, integer, pgTable, text, varchar } from "drizzle-orm/pg-core";
+import { date, integer, pgTable, pgEnum,text, varchar } from "drizzle-orm/pg-core";
 
-export const dataWargaTable = pgTable("users", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar({ length: 255 }).notNull(),
-    dob: date().notNull(),
-    nik: integer().notNull(),
+
+export const statusKawinEnum = pgEnum('status_kawin', ['belum kawin', 'kawin', 'cerai mati', 'cerai hidup']);
+export const jenisKelaminEnum = pgEnum('jenis_kelamin', ['laki-laki', 'perempuan']);
+export const hubunganKeluargaEnum = pgEnum('hubungan_keluarga',
+            ['Anak', 'Istri', 'Orang Tua', 'Kepala Keluarga',
+             'Cucu', 'Famili lain', 'Mertua', 'Menantu',
+             'Pembantu', 'Lainnya', 'Suami'
+            ]);
+export const pendidikanEnum = pgEnum('pendidikan', 
+            ['SLTA/Sederajat', 'Tidak/Belum Sekolah', 'Belum Tamat SD/Sederajat',
+             'SLTP/Sederajat', 'Strata III', 'Diploma IV/Strata I', 'Akademi/Diploma III/ Sarjana Muda',
+             'Tamat SD/Sederajat', 'Strata-II'
+            ]);
+
+export const dataWargaTable = pgTable("data_warga", {
+    // id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    nik: integer().notNull().primaryKey(),
+    nama_art: varchar({ length: 255 }).notNull(),
+    nama_kk: varchar({ length: 255 }).notNull(),
+    hubungan_keluarga: hubunganKeluargaEnum().notNull(),
+    alamat:text().notNull(),
+    tgl_lahir: date().notNull(),
     rt: integer().notNull(),
     rw: integer().notNull(),
+    kecamatan: text().notNull(),
+    kelurahan: text().notNull(),
+    kota: text().notNull(),
+    status_kawin: statusKawinEnum().notNull(),
     petugas: text().notNull(),
-    email: varchar({ length: 255 }).notNull().unique(),
+    jenis_kelamin: jenisKelaminEnum().notNull(),
+    agama: text().notNull(),
+    pendidikan: pendidikanEnum().notNull(),
+    pekerjaan: text().notNull(),
+});
+
+export const surveyor = pgTable("surveyor", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    nama: varchar({ length: 255 }).notNull(),
+});
+
+export const riawaySurvey = pgTable("riwayat_survey", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    nama: varchar({ length: 255 }).notNull(),
+    
 });
