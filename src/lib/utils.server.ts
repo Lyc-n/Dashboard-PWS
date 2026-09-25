f96e05b3fe51709e0e922c6b4c0702c7fe9960ad
arya-satwika <aryastwk95@gmail.com>
2026-09-25 22:33:25 +0700

    wip: login pake pin
    logic session getter dan setter udh jadi, kurang form submission logic
    buat validasi pin

--- src/lib/utils.server.ts
+++ src/lib/utils.server.ts
@@ -1,7 +1,7 @@
 import { db } from './db.server'
 import { riwayatSurvey, validSession } from './schema'
 import { jwtVerify, SignJWT } from 'jose'
-import { randomBytes } from 'node:crypto';
+import { randomBytes, verify } from 'node:crypto';
 import { setCookie } from '@tanstack/react-start/server';
 
 
@@ -12,13 +12,30 @@ import { setCookie } from '@tanstack/react-start/server';
 4. store session di cookie (expire in 1 hour) [X]
 */
 
-async function createSessionToken(){
+
+const secretKey = Buffer.from(import.meta.env.VITE_SECRET_KEY,"base64")
+
+
+
+export async function isValidPin(pin:number){
+    if(pin === import.meta.env.VITE_PIN){
+        setCookie('session', await createSessionHelper(), {
+            httpOnly: true,
+            secure: true,
+            path: '/',
+            maxAge: 60 * 60
+        })
+        return true
+    } else{ throw new Error("PIN is Not Valid") }
+
+}
+
+async function createSessionHelper(){
     const tokenPayload = {
         clientUUID: randomBytes(32).toString("base64url"), // mastiin  sessionnya unique per client
         loggedInAt: new Date().toISOString,
     };
 
-    const secretKey = Buffer.from(import.meta.env.VITE_SECRET_KEY,"base64")
     
     const token = await new SignJWT(tokenPayload)
         .setProtectedHeader({ alg: 'HS256' })
@@ -28,28 +45,22 @@ async function createSessionToken(){
     return token;
 }
 
-export async function isValidPin(pin:number){
-    if(pin === import.meta.env.VITE_PIN){
-        setCookie('session', await createSessionToken(), {
-            httpOnly: true,
-            secure: true,
-            path: '/',
-            maxAge: 60 * 60
-        })
-        return true
-    } else{ return false }
-
-}
-
-export async function isValidSession(token: string){
-    await db.query.validSession.findFirst({
+export async function getSessionHelper(sessionToken: string){
+    const sessionDb = await db.query.validSession.findFirst({
         where: {
-            token: token
+            token: sessionToken
         }
     })
+    if(!sessionDb){ throw new Error("Session does not exist")}
+    return sessionDb
 }
 
-export async function getAllSurveyData() {
+export async function verifyTokenHelper(sessionToken:string) {
+    const { payload }  = await jwtVerify(sessionToken, secretKey, { algorithms: ["HS256"]})
+    return payload
+}
+
+export async function queryAllSurveyData() {
   return await db.query.riwayatSurvey.findMany()
 }
 
--- src/routes/pin.tsx
+++ src/routes/pin.tsx
@@ -1,9 +1,34 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
+import { getSessionToken } from '#/lib/utils.functions'
+
+/* ALUR LOGIN
+1. cek sessionToken pake beforeLoad di /laporan (form)
+2. sessionToken di crosscheck ke validSessions di DB
+3. kalo gk ada kredensial redirect ke pin
+4. pin valid redirect ke /laporan
+5. update expireTime sessionToken kalo akses /laporan
+*/
 
 export const Route = createFileRoute('/pin')({
   component: RouteComponent,
+  beforeLoad: async () => {
+    try {
+      await getSessionToken();
+      throw redirect({ to: '/laporan' })
+    } catch (error) {
+      throw redirect({ to: '/' })
+    }
+  },
 })
 
 function RouteComponent() {
-  return <div>Hello "/pin"!</div>
+  return( 
+  <div>
+    <div> PIN PAD </div>
+    <form action="">
+      <input type="number" />
+    </form>
+    Hello "/pin"!
+  </div>
+  )
 }
